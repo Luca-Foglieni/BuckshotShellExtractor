322b1a4a984d1f57eba3f9cca68eed88ae37b6d9
Luca Foglieni <luca.foglieni.work648@passfwd.com>
2025-07-09 19:57:42 +0200

    Added icon and fixed app name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:buckshot_shell_ectractor/main.dart';
+import 'package:buckshot_shell_extractor/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
